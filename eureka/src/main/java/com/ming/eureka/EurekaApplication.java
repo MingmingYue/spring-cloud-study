d5f682809dc4dda84219fe48bd8c6ecea51c1a35
zhoumingming <zhoumingming@1zhen.com>
2019-06-02 22:44:27 +0800

    first commit

---
+++ eureka/src/main/java/com/ming/eureka/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.ming.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author: Ming
+ * @data: Create on 2019-06-02.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
