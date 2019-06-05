f0fbf48ca19f4821a51a7a62786b1a8ba1c91d20
zhoumingming <zhoumingming@1zhen.com>
2019-06-05 23:04:51 +0800

    基础提交

--- zuul-gateway/src/main/java/com/ming/zuul/gateway/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/com/ming/zuul/gateway/ZuulGatewayApplication.java
@@ -3,15 +3,13 @@ package com.ming.zuul.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author: Ming
  * Created by 2019-06-03
  */
 @EnableDiscoveryClient
+@EnableAceAuthClient
 
 @SpringBootApplication
 public class ZuulGatewayApplication {
---
+++ zuul-gateway/src/main/java/com/ming/zuul/gateway/conf/GatewayConfig.java
@@ -0,0 +1,17 @@
+package com.ming.zuul.gateway.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: Ming
+ * @data: Create on 2019-06-05.
+ */
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    RequestBodyRoutePredicateFactory requestBodyRoutePredicateFactory() {
+        return new RequestBodyRoutePredicateFactory();
+    }
+}
